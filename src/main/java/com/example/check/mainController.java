af581a22cf03a8ec028caf8aecce7fbd97a97c99
Clah7 <darrenliharja@gmail.com>
2023-11-26 23:56:08 +0700

    Mengumpulkan XML untuk page dalam

---
+++ src/main/java/com/example/check/mainController.java
@@ -0,0 +1,42 @@
+package com.example.check;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+public class mainController implements  Initializable{
+
+//    @FXML
+//    private BorderPane bp;
+//
+//    @FXML
+//    private AnchorPane main;
+//
+//    @Override
+//    public void initialize(URL url, ResourceBundle rb){
+//
+//    }
+//
+//    @FXML
+//    private void homePage(MouseEvent event){
+//    }
+//
+//    private void loadPage(String page){
+//        Parent root = null;
+//
+//        try{
+//            root = FXMLLoader.load(getClass().getResource(page+".fxml"));
+//        } catch(IOException ex){
+//            System.Logger.getLogger(mainController.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        main.setCenter(root);
+//    }
+}
